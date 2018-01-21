a5b3d5f0c0ac4e76a1e1e32805e643ed53edf9e6
Noah <noah.sommerfeld@gmail.com>
2018-01-21 13:50:43 -0800

    Modified code to use temperature sensor

--- Greenhouse_control/Greenhouse_control.ino
+++ Greenhouse_control/Greenhouse_control.ino
@@ -1,27 +1,20 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
- //extra comment
- //another extra comment
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 12;
+#include <dht.h>
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+dht DHT;
+
+#define DHT11_PIN 5
+
+void setup(){
+  Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  delay(3000);
 }
+
